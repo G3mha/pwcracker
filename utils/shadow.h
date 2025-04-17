0a20098455da44901fd7fe9f189114b3d7684211
Enricco Gemha <enriccogemha@gmail.com>
2025-04-16 22:33:31 -0400

    refactor: Reorganize file structure

--- utils/shadow.h
+++ utils/shadow.h
@@ -40,16 +40,6 @@ hash_type_t detect_hash_type(const shadow_entry_t *entry);
 char *extract_salt(const shadow_entry_t *entry);
 
 /**
- * Hash a password with the specified algorithm and salt
- *
- * @param password Plain text password
- * @param salt Salt to use
- * @param type Hash type
- * @return Hashed password (must be freed by caller), NULL on error
- */
-char *hash_password(const char *password, const char *salt, hash_type_t type);
-
-/**
  * Check if a password matches a shadow entry
  *
  * @param entry Shadow entry
